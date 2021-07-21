d25f91694ea6e143f858cde6f810325e545032c6
Amado Azua III <amado.azua3@gmail.com>
2021-07-21 10:59:04 -0500

    Maven exercise

---
+++ src/main/java/MavenApp.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Something: ");
+        String input = scanner.nextLine();
+
+        System.out.println("You entered: " + '"' + input + '"');
+
+        if(StringUtils.isNumeric(input)){
+            System.out.println('"' + input + '"' + " is a number");
+        } else if(!StringUtils.isNumeric(input)){
+            System.out.println('"' + input + '"' + " is not a number");
+        }
+
+        System.out.println("Flipped Case: "+ StringUtils.swapCase(input));
+
+        System.out.println("Reversed: " + StringUtils.reverse(input));
+
+    }
+
+}
